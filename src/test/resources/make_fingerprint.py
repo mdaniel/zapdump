0ed8d62ee24f50dd1a1ed3d836c63cc6929734d7
Matthew L Daniel <mdaniel@gmail.com>
2015-01-07 21:50:39 -0800

    Fixup some pycharm warnings

--- src/test/resources/make_fingerprint.py
+++ src/test/resources/make_fingerprint.py
@@ -7,20 +7,23 @@ __req._fingerprint_cache = collections.defaultdict()
 
 class Request(object):
     __slots__ = ['headers', 'method', 'url', 'body']
+
     def __init__(self, url, body):
         self.headers = dict()
         self.method = 'GET'
         self.url = url
         self.body = body
 
-the_url = sys.argv[1]
-body = ''
-if len(sys.argv) > 2:
-    the_dir = sys.argv[2]
-    body_file = '%s/request_body' % the_dir
-    if os.path.isfile(body_file):
-        with open(body_file, 'rb') as fh:
-            body = fh.read()
-req = Request(the_url, body)
-output = request_fingerprint(req)
-print('output := %r' % output)
+
+def main():
+    the_url = sys.argv[1]
+    body = ''
+    if len(sys.argv) > 2:
+        the_dir = sys.argv[2]
+        body_file = '%s/request_body' % the_dir
+        if os.path.isfile(body_file):
+            with open(body_file, 'rb') as fh:
+                body = fh.read()
+    req = Request(the_url, body)
+    output = request_fingerprint(req)
+    print('output := %r' % output)
